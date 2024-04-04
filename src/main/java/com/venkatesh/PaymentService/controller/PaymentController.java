485a8d0a823078f66070db0a5bb0d3a4ce92ea76
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-04 15:54:16 -0400

    Repository name change

--- src/main/java/com/venkatesh/PaymentService/controller/PaymentController.java
+++ src/main/java/com/venkatesh/PaymentService/controller/PaymentController.java
@@ -1,9 +1,16 @@
 package com.venkatesh.PaymentService.controller;
 
+import com.venkatesh.PaymentService.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    public ResponseEntity<Long> doPayment()
 }
--- src/main/java/com/venkatesh/PaymentService/repository/TransactionDetailsRepository.java
+++ src/main/java/com/venkatesh/PaymentService/repository/TransactionDetailsRepository.java
@@ -3,5 +3,5 @@ package com.venkatesh.PaymentService.repository;
 import com.venkatesh.PaymentService.entity.TransactionDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<TransactionDetails,Long> {
+public interface TransactionDetailsRepository extends JpaRepository<TransactionDetails,Long> {
 }
