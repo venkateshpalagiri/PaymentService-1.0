25fb74e685a774a61c4b631fcf20a6956f715e13
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-06 08:56:09 -0400

    Logs added

--- src/main/java/com/venkatesh/PaymentService/controller/PaymentController.java
+++ src/main/java/com/venkatesh/PaymentService/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.venkatesh.PaymentService.controller;
 
 import com.venkatesh.PaymentService.model.PaymentRequest;
 import com.venkatesh.PaymentService.service.PaymentService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment")
+@Log4j2
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest){
+        log.info("doPayment method called in PaymentController layer");
         return new ResponseEntity<>(paymentService.doPayment(paymentRequest), HttpStatus.OK);
     }
 }
--- src/main/java/com/venkatesh/PaymentService/service/PaymentServiceImpl.java
+++ src/main/java/com/venkatesh/PaymentService/service/PaymentServiceImpl.java
@@ -3,17 +3,20 @@ package com.venkatesh.PaymentService.service;
 import com.venkatesh.PaymentService.entity.TransactionDetails;
 import com.venkatesh.PaymentService.model.PaymentRequest;
 import com.venkatesh.PaymentService.repository.TransactionDetailsRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
 @Service
+@Log4j2
 public class PaymentServiceImpl implements PaymentService{
     @Autowired
     private TransactionDetailsRepository transactionDetailsRepository;
     @Override
     public long doPayment(PaymentRequest paymentRequest) {
+        log.info("doPayment method called in PaymentServiceImpl with payload:{}",paymentRequest);
         TransactionDetails transactionDetails=TransactionDetails
                 .builder()
                 .orderId(paymentRequest.getOrderId())
@@ -23,7 +26,9 @@ public class PaymentServiceImpl implements PaymentService{
                 .paymentStatus("SUCCESS")
                 .amount(paymentRequest.getAmount())
                 .build();
+        log.info("Transaction details created in PaymentServiceImpl with:{}",transactionDetails);
         transactionDetailsRepository.save(transactionDetails);
+        log.info("doPayment method executed in PaymentServiceImpl successfully");
         return transactionDetails.getId();
     }
 }
